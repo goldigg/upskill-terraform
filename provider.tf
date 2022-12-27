17dd568dd4ab777a40784bc720827615d69e04b4
ggoldmann-pgs <ggoldmann@pgs-soft.com>
2022-12-27 17:26:40 +0100

    Remote backend

--- provider.tf
+++ provider.tf
@@ -3,3 +3,12 @@ provider "aws" {
   shared_credentials_files = ["./credentials"]
   profile                  = "ggoldmann"
 }
+terraform {
+  backend "s3" {
+    encrypt        = true
+    bucket = "ggoldmann-tf-state"
+    key    = "path/to/my/key"
+    region = "eu-central-1"
+    dynamodb_table = "ggoldmann-terraform-state-lock"
+  }
+}
